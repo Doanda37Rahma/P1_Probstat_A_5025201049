5ea1fce0a591a2b640549b7826d120349e017e1e
Doanda Dresta Rahma <66405353+Doanda37Rahma@users.noreply.github.com>
2022-04-10 16:26:19 +0700

    fix soal2

--- soal2.r
+++ soal2.r
@@ -6,6 +6,8 @@ x = 0:20
 p <- dbinom(x, size=20, prob=0.2)
 data = data.frame(x=x, p=p)
 barplot(height=data$p, names.arg=data$x, xlab="# Pasien sembuh", ylab="Peluang")
+bp <- barplot(height=data$p, names.arg=data$x, xlab="# Pasien sembuh", ylab="Peluang")
+text(bp, data$p, labels=round(data$p, 2))
 
 # 2c
 n = 20
